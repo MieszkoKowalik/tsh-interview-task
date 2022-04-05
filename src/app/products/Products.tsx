85b6537a84a97a861fb51b27c5ff12bc0a31e703
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-04-05 12:46:38 +0200

    feat: Add Logo component

--- src/app/products/Products.tsx
+++ src/app/products/Products.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { SecondaryButton } from 'app/shared/SecondaryButton';
+import { Logo } from 'app/shared/Logo';
 
 import { AppRoute } from 'routing/AppRoute.enum';
 
 export const Products = () => {
   return (
     <>
-      <h2>Products page</h2>
+      <Logo />
       <SecondaryButton as={Link} to={AppRoute.Login}>
         Log in
       </SecondaryButton>
