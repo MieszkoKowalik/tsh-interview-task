096cff728554d7ce0d30ca396dc40ceb2bf248db
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-04-05 11:08:34 +0200

    feat: Add GlobalStyle

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'assets/styles/theme';
+import { GlobalStyle } from 'assets/styles/globalStyle';
 
 import { AppProvidersProps } from './AppProviders.types';
 
 export const AppProviders = ({ children }: AppProvidersProps) => (
   <Router>
-    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      {children}
+    </ThemeProvider>
   </Router>
 );
