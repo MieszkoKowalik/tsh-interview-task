44b254322b68f6dbe84b8dc2b314f6535dbaa622
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2022-04-12 17:42:57 +0200

    test: Add auth logout assertion

--- e2e/cypress/integration/auth/auth.test.ts
+++ e2e/cypress/integration/auth/auth.test.ts
@@ -12,10 +12,14 @@ describe('User auth', () => {
     cy.get('input:invalid').should('have.length', 2);
   });
 
-  it('Can Log in a user', () => {
+  it('Can Log in and log out a user', () => {
     cy.findByLabelText('Username').type('string');
     cy.findByLabelText('Password').type('string');
 
     cy.findByText('Log in').click();
+
+    cy.findByLabelText('Menu').click();
+    cy.findByText('Logout').click();
+    cy.findByText('Log in').should('exist');
   });
 });
